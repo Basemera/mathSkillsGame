f7118ab9276e4f082ed52e539e96e16104a0567d
Basemera <phiona.basemera@andela.com>
2018-10-01 20:33:53 +0300

    Add linting

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 #### MathSkillsGame
 A game to challenge you in math
+#### Instructions
+-Pick a number whose sum is the number of stars displayed
+-Then select the = sign.
+-Your answer is correct you will get a check, after which you can go ahead and click the check mark to accept the answer
+-If your answer is wrong, you will get an x and you can select another set of numbers and try again
+-Should you wish to have another set of stars click the yellow button with the counter to refresh. You have 5 draws.
+-Happy gaming
---
+++ src/components/number.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Number = props => {
+  Number.list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numberClassName = number => {
+    if (props.usedNumbers.indexOf(number) >= 0) {
+      return "used";
+    }
+    if (props.selectedNumbers.indexOf(number) >= 0) {
+      return "selected";
+    }
+  };
+  return (
+    <div className="card text-center col-4">
+      <div>
+        {Number.list.map((number, i) => (
+          <span
+            key={i}
+            className={numberClassName(number)}
+            onClick={() => props.addSelectedNumber(number)}
+          >
+            {number}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Number;
